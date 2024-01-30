1eb555b7de2ebc496d8959ae2ecc56af56f63b7c
schloing <user.datagram.protocol@proton.me>
2024-01-30 20:00:08 +1100

    simple print-related features, moving on to disk

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+build + run with qemu: `nasm main.s -f bin -o boot_sect.bin; qemu-system-x86_64 -nographic boot_sect.bin`  
 https://www.cs.bham.ac.uk/~exr/lectures/opsys/10_11/lectures/os-dev.pdf
---
+++ bs_print.s
@@ -0,0 +1,69 @@
+print:
+    pusha
+
+start:
+    mov al, [bx]
+    cmp al, 0
+    je done
+
+    mov ah, 0x0e
+    int 0x10
+
+    inc bx
+    jmp start
+
+done:
+    popa
+    ret
+
+print_nl:
+    pusha
+    
+    mov ah, 0x0e
+
+    mov al, 0x0a
+    int 0x10
+
+    mov al, 0x0d
+    int 0x10
+
+    popa
+    ret
+
+print_hex:
+    pusha
+
+    mov cx, 4
+
+loop:
+    dec cx
+
+    mov bx, HEX
+    add bx, 2
+    add bx, cx
+
+    mov ax, dx
+    shr dx, 4
+    and ax, 0x0F
+
+    cmp ax, 0xA
+    
+    jl letter
+    add byte [bx], 7
+    jl letter
+
+letter:
+    add byte [bx], al
+
+    cmp cx, 0
+    je end
+
+    jmp loop
+
+end:
+    call print
+
+    popa
+    ret
+
+HEX: db "0x0000", 0
